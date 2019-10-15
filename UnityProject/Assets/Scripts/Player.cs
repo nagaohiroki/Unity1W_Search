ce6e17a1a80c8211726f93a77822a268a7b14110
nagaohiroki <testplayers0226@gmail.com>
2019-10-16 08:33:13 +0900

    プレイヤー追加

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	float mMoveSpeed = 0.0f;
+	[SerializeField]
+	float mRotateSpeed = 0.0f;
+	void Update()
+	{
+		var vec = Vector3.zero;
+		vec.x = Input.GetAxis("LeftHorizontal");
+		vec.z = Input.GetAxis("LeftVertical");
+		var rigid = GetComponent<Rigidbody>();
+		var forw = transform.rotation * vec * Time.deltaTime * mMoveSpeed;
+		rigid.MovePosition(transform.position + forw);
+	}
+}
