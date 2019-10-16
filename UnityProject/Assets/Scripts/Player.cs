7555d31140a3f5e2ee5b149f6a6e32d4efd6c024
nagaohiroki <testplayers0226@gmail.com>
2019-10-17 08:33:53 +0900

    プレイヤー作成

--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -5,13 +5,26 @@ public class Player : MonoBehaviour
 	float mMoveSpeed = 0.0f;
 	[SerializeField]
 	float mRotateSpeed = 0.0f;
+	[SerializeField]
+	Rigidbody mRigidbody = null;
+	[SerializeField]
+	Transform mCamera = null;
 	void Update()
 	{
-		var vec = Vector3.zero;
-		vec.x = Input.GetAxis("LeftHorizontal");
-		vec.z = Input.GetAxis("LeftVertical");
-		var rigid = GetComponent<Rigidbody>();
-		var forw = transform.rotation * vec * Time.deltaTime * mMoveSpeed;
-		rigid.MovePosition(transform.position + forw);
+		// 移動
+		var move = Vector3.zero;
+		move.x = Input.GetAxis("LeftHorizontal");
+		move.z = -Input.GetAxis("LeftVertical");
+		var velocity = transform.rotation * move * Time.deltaTime * mMoveSpeed;
+		mRigidbody.MovePosition(transform.position + velocity);
+		// 旋回
+		var angle = Vector3.zero;
+		angle.y = Input.GetAxis("RightHorizontal");
+		var rot = transform.rotation.eulerAngles + angle * Time.deltaTime * mRotateSpeed;
+		mRigidbody.MoveRotation(Quaternion.Euler(rot));
+		var xangle = Vector3.zero;
+		xangle.x = -Input.GetAxis("RightVertical");
+		var xrot = mCamera.transform.rotation.eulerAngles + xangle * Time.deltaTime * mRotateSpeed;
+		mCamera.transform.rotation = Quaternion.Euler(xrot);
 	}
 }
